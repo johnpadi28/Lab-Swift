ba9aab4ec662b8d604fab7c59172be923efc6fa2
John Padilla <johnpadi28@gmail.com>
2020-10-01 09:43:19 -0600

    OrderOfevents1

--- OrderOfEvents/OrderOfEvents/MiddleViewController.swift
+++ OrderOfEvents/OrderOfEvents/MiddleViewController.swift
@@ -8,22 +8,48 @@
 import UIKit
 
 class MiddleViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var eventNumber: Int = 1
+    
+    func addEvent(from: String) {
+        if let existingText = label.text {
+            label.text = "\(existingText) \nEvent number \(eventNumber) was \(from)"
+            eventNumber += 1
+        }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        addEvent(from: "viewDidLoad")
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addEvent(from: "ViewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        addEvent(from: "ViewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        addEvent(from: "ViewWillDesappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        addEvent(from: "ViewDidDisappear")
+    }
+    
 }
