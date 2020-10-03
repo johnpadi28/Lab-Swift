664c5cf9fa6543dd4a3f626e0995819007194232
John Padilla <johnpadi28@gmail.com>
2020-10-02 21:49:07 -0600

    Pizza Delivery

---
+++ Pizza Delivery/Pizza Delivery/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  Pizza Delivery
+//
+//  Created by John Padilla on 10/2/20.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Pizza Delivery/Pizza Delivery/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Pizza Delivery
+//
+//  Created by John Padilla on 10/2/20.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
