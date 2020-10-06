224e7639c08aceba6dc168d3dfe6b0d3057a4413
John Padilla <johnpadi28@gmail.com>
2020-10-06 02:03:06 -0600

    Personality Quiz

---
+++ PersonalQuiz/PersonalQuiz/IntroductionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by John Padilla on 10/5/20.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalQuiz/PersonalQuiz/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by John Padilla on 10/5/20.
+//
+
+import Foundation
