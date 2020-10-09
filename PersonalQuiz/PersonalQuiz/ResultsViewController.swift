5216990bb5ab1fdaff51d5c8315b44bbbb20ebe1
John Padilla <johnpadi28@gmail.com>
2020-10-09 09:21:00 -0600

    ISpy

--- PersonalQuiz/PersonalQuiz/ResultsViewController.swift
+++ PersonalQuiz/PersonalQuiz/ResultsViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var responses: [Answer]
+    
+    @IBOutlet var resultAnswerLabel: UILabel!
+    @IBOutlet var resultDefinitionLabel: UILabel!
+    
+    init?(coder: NSCoder, responses: [Answer]) {
+        self.responses = responses
+        super.init(coder: coder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        calculatePersonalityResult()
+        navigationItem.hidesBackButton = true
+
+    }
+    
+    func calculatePersonalityResult() {
+        let frequencyOfAnswers = responses.reduce(into: [:]) { (counts, answer) in
+            counts[answer.type, default: 0] += 1
+        }
+        
+//        let frequentAnswerSorted = frequencyOfAnswers.sorted { (part1, pair2) -> Bool in
+//            return part1.value > pair2.value
+//        }
+        
+        let mostCommonanswer = frequencyOfAnswers.sorted {$0.1 > $1.1}.first!.key
+        
+        resultAnswerLabel.text = "You are a \(mostCommonanswer.rawValue)!"
+        resultDefinitionLabel.text = mostCommonanswer.definition
+    }
+    
 
 }
