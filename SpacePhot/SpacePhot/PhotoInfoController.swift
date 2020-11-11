4d9fa349836202157292c992c3d96354fc6bc880
John Padilla <johnpadi28@gmail.com>
2020-11-11 11:59:35 -0700

    SpacePhoto

---
+++ SpacePhot/SpacePhot/PhotoInfoController.swift
@@ -0,0 +1,29 @@
+//
+//  PhotoInfoController.swift
+//  SpacePhot
+//
+//  Created by John Padilla on 11/11/20.
+//
+
+import Foundation
+
+class PhotoInfoController {
+    func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void)
+    {
+        let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+        
+        let query: [String: String] = ["api_key": "DEMO_KEY"]
+        
+        let url = baseURL.withQueries(query)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
+                completion(photoInfo)
+            } else {
+                print("Either no data was returned, or data was not serialized.")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
